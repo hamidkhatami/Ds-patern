0fd6d9060c9317af6b84c89b351804d5995dc479
khatami <Kk@123456>
2023-04-08 16:58:03 +0330

    singleton for best memory usage

---
+++ main/java/com/singletone/MemoryUseSingletonInstance.java
@@ -0,0 +1,6 @@
+package main.java.com.singletone; /**
+  * @author Hamid Khatami (Khatami@caspco.ir)
+  * @version 1.0 2023.0408
+  * @since 1.0
+  */public final class MemoryUseSingletonINstance {
+}
