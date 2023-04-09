4468697b1b2594f94850ceb817c3bfc88f97a3ec
khatami <Kk@123456>
2023-04-09 11:52:31 +0330

    Serialize Sngleton

--- main/java/com/Main.java
+++ main/java/com/Main.java
@@ -2,6 +2,7 @@ package main.java.com;
 
 import main.java.com.singletone.LazySingletonInstance;
 import main.java.com.singletone.MemoryUseSingletonInstance;
+import main.java.com.singletone.SerializeSingleton;
 import main.java.com.singletone.SingletonInstance;
 import main.java.com.singletone.StaticBlockInstance;
 import main.java.com.singletone.ThreadSafeSingletonInstance;
@@ -11,29 +12,36 @@ public class Main {
     SingletonInstance singletonInstance = SingletonInstance.mySingleton();
     singletonInstance.hello();
 
-    System.out.println("--------------------------------");
+    System.out.println("-------------Simple -------------------");
     System.out.println("              ");
 
     StaticBlockInstance staticBlockInstance = StaticBlockInstance.instance();
     staticBlockInstance.hello();
 
-    System.out.println("--------------------------------");
+    System.out.println("--------------Lazy ------------------");
     System.out.println("              ");
 
     LazySingletonInstance lazySingletonInstance = LazySingletonInstance.instance();
     lazySingletonInstance.hello();
 
-    System.out.println("--------------------------------");
+    System.out.println("---------------Thread Safe-----------------");
     System.out.println("              ");
 
     ThreadSafeSingletonInstance threadSafeSingletonInstance = ThreadSafeSingletonInstance.instance();
     threadSafeSingletonInstance.hello();
 
-    System.out.println("--------------------------------");
+    System.out.println("---------------Best Memory Usage-----------------");
     System.out.println("              ");
 
     MemoryUseSingletonInstance memoryUseSingletonInstance = MemoryUseSingletonInstance.myInstance();
     memoryUseSingletonInstance.hello();
+
+    System.out.println();
+    System.out.println("--------------Serials------------------");
+    System.out.println();
+
+    SerializeSingleton serializeSingleton = SerializeSingleton.myInstance();
+    serializeSingleton.hello();
 
 
   }
---
+++ main/java/com/singletone/SerializeSingleton.java
@@ -0,0 +1,25 @@
+package main.java.com.singletone;
+
+/**
+ * @author Hamid Khatami (Khatami@caspco.ir)
+ * @version 1.0 2023.0409
+ * @since 1.0
+ */
+public final class SerializeSingleton {
+  private static final long serialVersionUID = -7604766932017737115L;
+
+  private SerializeSingleton() {}
+
+  private static class SingletonHelper {
+    private static final SerializeSingleton instance = new SerializeSingleton();
+  }
+
+  public static SerializeSingleton myInstance() {
+    return SingletonHelper.instance;
+  }
+
+  public void hello(){
+    System.out.println("Hello World From Serial Singleton");
+  }
+
+}
